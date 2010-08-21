acb2d0d34394335881dd41902f41c3d8b37a8f5e
diogobor@gmail.com <diogobor@gmail.com@e49228ef-bd4d-a6a5-bad3-ecc770689445>
2010-08-21 12:31:49 +0000

    Criacao da camada de dominio Impl

--- trunk/SuportePais/src/br/gov/tutorial/cs/PaisHandlerImpl.java
+++ trunk/SuportePais/src/br/gov/tutorial/cs/PaisHandlerImpl.java
@@ -4,33 +4,28 @@ import java.util.Collection;
 import java.util.List;
 
 import br.gov.tutorial.cd.Pais;
-import br.gov.tutorial.view.cadastroPais.consultaPais.form.ConsultaPaisForm;
+import br.gov.tutorial.cd.PaisImpl;
 import br.gov.tutorial.vo.PaisVO;
 
 public class PaisHandlerImpl extends PaisHandler{
 
 	@Override
-	public Collection handleFilter(ConsultaPaisForm form) {
-		PaisVO paisvo = new PaisVO();
-		paisvo.setCodigo(form.getCodigo());
-		paisvo.setCodigoAuxiliar(form.getCodigoAuxiliar());
-		paisvo.setDescricao(form.getDescricao());
-		paisvo.setValor(form.getValor());
+	public Collection handleFilter(PaisVO paisVO) {
 		//Passar o pais vo para o buscar pais
-		List listaPais = dao.buscarPais("");
+		List listaPais = paisDao.buscarPais(paisVO);
 		return listaPais;
 	}
 
 	@Override
-	public void handleInsert(ConsultaPaisForm form) {
+	public void handleInsert(PaisVO paisVO) {
 		
-		Pais pais = new Pais();
-		pais.setCodigo(form.getCodigo());
-		pais.setCodigoAuxiliar(form.getCodigoAuxiliar());
-		pais.setDescricao(form.getDescricao());
-		pais.setValor(form.getValor());
+		Pais pais = new PaisImpl();
+		pais.setCodigo(paisVO.getCodigo());
+		pais.setCodigoAuxiliar(paisVO.getCodigoAuxiliar());
+		pais.setDescricao(paisVO.getDescricao());
+		pais.setValor(paisVO.getValor());
 		
-		dao.inserir(pais);
+		paisDao.inserir(pais);
 		
 	}
 
