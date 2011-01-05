94e8b789d3c56bcfaa6ed036136590d5ec1d2e5f
diogobor@gmail.com <diogobor@gmail.com@e49228ef-bd4d-a6a5-bad3-ecc770689445>
2011-01-05 00:15:22 +0000

    organizando arquivo

--- trunk/SuportePais/target/br/gov/tutorial/cs/PaisHandler.java
+++ trunk/SuportePais/target/br/gov/tutorial/cs/PaisHandler.java
@@ -11,24 +11,18 @@ import br.gov.tutorial.vo.PaisVO;
 public abstract class PaisHandler extends DataBase{
 	protected PaisDAO paisDao = null;
 	
-	public PaisHandler(){
-		if (paisDao == null)
-			paisDao = new PaisDAOImpl();
-	}
-	
 	protected abstract Collection handleFilter(PaisVO paisVO);
 	protected abstract void handleInsert(PaisVO paisVO);
 	protected abstract void handleExcluir(PaisVO paisVO);
 	protected abstract void handleAtualizar(PaisVO paisVO);
 	
-	public Collection Filter(PaisVO paisVO){
-		
+	public PaisHandler(){
+		if (paisDao == null)
+			paisDao = new PaisDAOImpl();
+	}	
+	public Collection Filter(PaisVO paisVO){	
 		List listaPais = (List) handleFilter(paisVO);
-		
 		return listaPais;
-		
-		
-		
 	}
 	public void Insert(PaisVO paisVO){
 		db.beginTransaction();
