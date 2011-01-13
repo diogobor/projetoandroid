c8cd5c14dc2c72909c4f5ddc0eef8d469004d697
diogobor@gmail.com <diogobor@gmail.com@e49228ef-bd4d-a6a5-bad3-ecc770689445>
2011-01-13 00:18:35 +0000

    adicionado os try catch

--- trunk/SuportePais/target/br/gov/tutorial/controller/DataBase.java
+++ trunk/SuportePais/target/br/gov/tutorial/controller/DataBase.java
@@ -2,9 +2,11 @@ package br.gov.tutorial.controller;
 
 import br.ufrj.dcc.api.controller.ConnectionDataBase;
 import br.ufrj.dcc.api.controller.ContextDataBase;
+import br.ufrj.dcc.api.view.PageFacade;
 import br.ufrj.dcc.impl.controller.ConnectionDataBaseImpl;
 import br.ufrj.dcc.impl.controller.ContextDataBaseImpl;
 import br.ufrj.dcc.impl.controller.PersistenceValuesImpl;
+import br.ufrj.dcc.impl.view.PageFacadeImpl;
 
 public class DataBase{
 	
@@ -16,19 +18,47 @@ public class DataBase{
 	
 	public DataBase (){
 		
-		if (context == null)
-			context = new ContextDataBaseImpl(NOME_BANCO, VERSAO_BANCO);
-		if (connection == null)
-			connection = new ConnectionDataBaseImpl(((ContextDataBaseImpl)context));
+		if (context == null){
+			try {
+				context = new ContextDataBaseImpl(NOME_BANCO, VERSAO_BANCO);	
+			} catch (Exception e) {
+				PageFacade page = new PageFacadeImpl();
+				page.showErrorMessage("Erro ao criar o Banco!");
+			}
+		}
+		if (connection == null){
+			try {
+				connection = new ConnectionDataBaseImpl(((ContextDataBaseImpl)context));
+			} catch (Exception e) {
+				PageFacade page = new PageFacadeImpl();
+				page.showErrorMessage("Erro ao conectar ao Banco!");
+			}
+		}
 		if (db == null){
 			db = new PersistenceValuesImpl(connection);
 		}
 	}
 	
 	public void executeSQL(String sql){
-		connection = new ConnectionDataBaseImpl(((ContextDataBaseImpl)context));
+		if (connection == null){
+			try {
+				connection = new ConnectionDataBaseImpl(((ContextDataBaseImpl)context));
+			} catch (Exception e) {
+				PageFacade page = new PageFacadeImpl();
+				page.showErrorMessage("Erro ao conectar ao Banco!");
+			}
+		}
+		
 		context.insertSQL(connection,sql);
-		connection = new ConnectionDataBaseImpl(((ContextDataBaseImpl)context));
+		
+		if (connection == null){
+			try {
+				connection = new ConnectionDataBaseImpl(((ContextDataBaseImpl)context));
+			} catch (Exception e) {
+				PageFacade page = new PageFacadeImpl();
+				page.showErrorMessage("Erro ao conectar ao Banco!");
+			}
+		}
 		db = new PersistenceValuesImpl(connection);
 	}
 }
